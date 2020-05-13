9eca313c33fe9795e52060a16cfac222fead7251
Nuria <c.odena.nuria@gmail.com>
2020-05-13 20:22:46 +0200

    Lifecycle Hooks in Action

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,16 +1,52 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit,  AfterViewChecked, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit,  AfterViewChecked, OnDestroy {
   @Input('alias') element: {type: string, name: string, content: string};
+  @Input() name: string;
 
-  constructor() { }
+  constructor() {
+    console.log('constructor called')
+   }
+
+   ngOnChanges(changes: SimpleChanges) {
+     console.log('ngOnChanges called');
+     console.log(changes)
+   }
 
   ngOnInit(): void {
+    console.log('ngOnInit called!')
   }
+
+  ngDoCheck() {
+    console.log('ngDoCheck called')
+  }
+
+  ngAfterContentInit () {
+    console.log('ngAfterContentInit')
+  }
+
+  ngAfterContentChecked () {
+    console.log('ngAfterContentChecked')
+
+  }
+  ngAfterViewInit () {
+    console.log('AfterViewInit')
+  }
+
+  ngAfterViewChecked () {
+    console.log('AfterViewChecked')
+
+  }
+
+  ngOnDestroy () {
+    console.log('ngOnDestroy')
+
+  }
+
 
 }
